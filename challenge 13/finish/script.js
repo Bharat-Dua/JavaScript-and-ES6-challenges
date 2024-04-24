ba489fe7a9b6762941e1d1be30a69226ed259b35
Bharat Dua <bharatdua13@gmail.com>
2024-04-24 16:44:18 +0530

    done challenge

---
+++ challenge 13/finish/script.js
@@ -0,0 +1,34 @@
+/* CHALLENGE 13 - Template Literals
+
+Create carInfo() function that
+will return info about each car.
+
+Car is considered cheap if it's price is <= 20000
+Car is considered expensive it it's price is > 20000
+*/
+
+"use strict";
+
+var cars = [
+  { brand: "Honda", price: 13000 },
+  { brand: "Rolls-Royce", price: 120000 },
+];
+
+// Write carInfo() function here
+function carInfo(car) {
+  if (car.price <= 20000)
+    console.log(
+      `The ${car.brand} is a cheap car, priced at only $${car.price}.`
+    );
+  else
+    console.log(
+      `The ${car.brand} is an expensive car, priced at $${car.price}. It is not recommended for the budget conscious buyer.`
+    );
+}
+
+cars.forEach((car) => console.log(carInfo(car)));
+/* Price of my new Honda is 13000$
+ and it is cheap car. */
+
+/* Price of my new Rolls-Royce
+ is 120000$ and it is expensive car. */
