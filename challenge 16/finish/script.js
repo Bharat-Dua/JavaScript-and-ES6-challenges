837c46185ad9087216a0e3d444ce0470246add86
Bharat Dua <bharatdua13@gmail.com>
2024-04-24 17:18:23 +0530

    done challenge

---
+++ challenge 16/finish/script.js
@@ -0,0 +1,24 @@
+/* CHALLENGE 16 - Swap values of the two variables
+
+Swap values of the a and b.
+Don't use for this any new variable.
+*/
+
+"use strict";
+
+var a = "first";
+var b = "second";
+
+// Write code here
+//* method 1
+let temp = a;
+a = b;
+b = temp;
+
+//* method 2
+[a, b] = [b, a]
+
+//* method 3
+[b,a]=[a,b]
+console.log(a, b);
+// second first
